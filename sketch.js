7f3cc8b7efba2bdb271268a86766666114d081c9
JoseLohan <93736740+JoseLohan@users.noreply.github.com>
2021-11-04 17:00:33 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background("Red");
+  textSize (20);
+  fill ("blue");
+  textAlign(CENTER,CENTER);
+  text("Hello World",200,200)
+}
+
+
+
+
